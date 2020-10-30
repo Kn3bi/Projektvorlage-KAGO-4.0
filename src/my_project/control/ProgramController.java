5a5bb6d317eb966a485387e3b167adc5d20006d3
knb <rma6VBRe!00>
2020-10-30 10:30:56 +0100

    Weitere Aufräumarbeiten.

--- src/my_project/control/ProgramController.java
+++ src/my_project/control/ProgramController.java
@@ -1,10 +1,6 @@
 package my_project.control;
 
 import KAGO_framework.control.ViewController;
-import my_project.model.Player;
-import my_project.view.PlayView;
-import my_project.view.PlayerSelectView;
-import my_project.view.StartView;
 
 import javax.swing.*;
 import java.awt.event.MouseEvent;
@@ -16,21 +12,10 @@ import java.awt.event.MouseEvent;
 public class ProgramController {
 
     //Attribute
-    private int targetPort;
+
 
     // Referenzen
     private ViewController viewController;  // diese Referenz soll auf ein Objekt der Klasse viewController zeigen. Über dieses Objekt wird das Fenster gesteuert.
-    private SSPNetworkController SSPNetworkController;
-    private StartView startView;
-    private PlayerSelectView playerSelectView;
-    private PlayView playView;
-    private State state;
-    private Player player;
-
-    // Enum
-    public enum State {
-        SCANNING, TRYAGAIN, PLAYERSELECT, WAITINGFORNAME, PLAYING, FINISHED
-    }
 
     /**
      * Konstruktor
@@ -47,18 +32,7 @@ public class ProgramController {
      * Diese Methode wird genau ein mal nach Programmstart aufgerufen. Achtung: funktioniert nicht im Szenario-Modus
      */
     public void startProgram() {
-        SSPNetworkController = new SSPNetworkController(this);
-        while(targetPort < 1000 || targetPort > 65000) {
-            try {
-                String m = JOptionPane.showInputDialog("Port für Spielserver (1000 bis 65000) eingeben:");
-                targetPort = Integer.parseInt(m);
 
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(viewController.getDrawFrame(), "Wer keine Zahl eingeben kann, darf nicht spielen.", "Braincheck", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-        startView = new StartView(viewController,this);
-        setState(State.SCANNING);
     }
 
     /**
@@ -68,117 +42,18 @@ public class ProgramController {
      * @param dt Zeit seit letzter Frame
      */
     public void updateProgram(double dt){
-        if(state == State.SCANNING){
-            if (SSPNetworkController.getServerIP() != null){
-                if (SSPNetworkController.getServerIP().equals("timeout")){
-                    startView.displayTryAgain();
-                    setState(State.TRYAGAIN);
-                } else {
-                    setState(State.PLAYERSELECT);
-                }
-            } else {
-                startView.displayScanning();
-            }
-        }
+
     }
+
 
     /**
-     * Ändert den Spielzusand
-     * @param state ein im enum State definierter Zustand
+     * Verarbeitet einen Mausklick.
+     * @param e das Objekt enthält alle Informationen zum Klick
      */
-    public void setState(State state){
-        this.state = state;
-        if (state == State.SCANNING) SSPNetworkController.startNetworkScan(targetPort);
-        if (state == State.PLAYERSELECT){
-            startView.disposeView();
-            SSPNetworkController.startConnection();
-            playerSelectView = new PlayerSelectView(viewController,this);
-        }
-        if (state == State.PLAYING){
-            player = new Player(playerSelectView.getName());
-            playerSelectView.disposeView();
-            playView = new PlayView(viewController,this,playerSelectView.getPlayerIcons(),playerSelectView.getSelectedIconIndex(),player.getName(),player.getPunkte());
-            SSPNetworkController.sendPlayerName(player);
-       }
+    public void mouseClicked(MouseEvent e){
+
     }
 
-    public void mouseClicked(MouseEvent e){}
 
-    public void sendSelectionToServer(int selectedIndex){
-        SSPNetworkController.sendPlayerChoice(convertNumberToLetter(selectedIndex));
-    }
-
-    public void requestSelectionFromPlayer(){
-        playView.activateChoosing();
-    }
-
-    public void verarbeiteGegnernamen(String name){
-        playView.setNextEnemy(name);
-    }
-
-    public void verarbeiteGegnerauswahl(String auswahl){
-        playView.setEnemyChoice(convertLetterToNumber(auswahl));
-    }
-
-    public void verarbeiteNeuePunkte(String punkte){
-        try{
-            int newPoints = Integer.parseInt(punkte);
-            if(newPoints > player.getPunkte() + 1){
-                playView.showRoundAnimation(PlayView.RoundAnimation.WINNING);
-            } else if (newPoints == player.getPunkte()+1){
-                playView.showRoundAnimation(PlayView.RoundAnimation.DRAW);
-            } else {
-                playView.showRoundAnimation(PlayView.RoundAnimation.LOOSING);
-            }
-            player.setPunkte(newPoints);
-            playView.setPlayerPoints(newPoints);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    private int convertLetterToNumber(String a){
-        switch (a){
-            case "A": return 0;
-            case "B": return 1;
-            case "C": return 2;
-            case "D": return 3;
-            case "E": return 4;
-        }
-        return -1;
-    }
-
-    private String convertNumberToLetter(int a){
-        switch(a){
-            case 0: return "A";
-            case 1: return "B";
-            case 2: return "C";
-            case 3: return "D";
-            case 4: return "E";
-        }
-        return "";
-    }
-
-    public void verarbeiteNeuenStatus(String status){
-        if(status.equals("gewonnen")){
-
-        }
-        if(status.equals("verloren")){
-
-        }
-        if(status.equals("aussetzen")){
-
-        }
-        if(status.startsWith("wartenAuf")){
-
-        }
-        if(status.equals("startDesTurniers")){
-
-        }
-    }
 
 }
