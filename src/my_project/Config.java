546c3af57e0b059e7e77eabc2e10af9c6b6d229c
M.K <knb@kkg-do.eu>
2020-11-22 13:04:09 +0100

    SQL-Funktionalität - Database-Controller hinzugefügt   - unterstützt automatische Verbindung zu einer SQL-Datenbank   - Methoden sonst identisch zu Abitur-Schnittstelle

--- src/my_project/Config.java
+++ src/my_project/Config.java
@@ -7,9 +7,10 @@ package my_project;
 public class Config {
 
     // Titel des Programms (steht oben in der Fenstertitelzeile)
-    public final static String WINDOW_TITLE = "Unbenanntes KAGO-4.0b-Projekt";
+    public final static String WINDOW_TITLE = "Unbenanntes KAGO-4.1b-Projekt";
 
-    // Breite des Programmfensters (Width) und Höhe des Programmfensters (Height)
+    // Konfiguration des Standardfensters: Anzeige und Breite des Programmfensters (Width) und Höhe des Programmfensters (Height)
+    public final static boolean SHOW_DEFAULT_WINDOW = true;
     public final static int WINDOW_WIDTH = 600;
     public final static int WINDOW_HEIGHT = 600+29;   // Effektive Höhe ist etwa 29 Pixel geringer (Titelleiste wird mitgezählt)
 
